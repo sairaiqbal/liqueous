de7edda058f0473786ec264411f93c0099afaf85
hammadmalik956 <hammadhameed956@gmail.com>
2023-08-04 16:24:13 +0500

    Going into the space

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      Hello I am About Page 
+    </div>
+  );
+}
+
+export default About;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   
+   <h1 className=' text-center  font-medium text-xl p-2 ' >Hello I am ME</h1>
+   
+  )
+}
